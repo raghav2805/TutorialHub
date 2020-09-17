3be5363d20d29785b57639f263461904169596fa
kartik27b <kabby272001@gmail.com>
2020-09-17 14:00:31 +0530

    added hosts

--- tutorialhub/tutorialhub/settings.py
+++ tutorialhub/tutorialhub/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+p9h%ki-jl#-@mth@xe991dyfva%66q70$3qd=am)j1uq@s2d-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

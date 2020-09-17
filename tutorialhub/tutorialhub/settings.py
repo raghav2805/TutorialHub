8c3bc6100e1f0995dd77adbaabc0fe22aa96a747
kartik27b <kabby272001@gmail.com>
2020-09-17 17:31:30 +0530

    added database host

--- tutorialhub/tutorialhub/settings.py
+++ tutorialhub/tutorialhub/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'tutorialhub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorialhubdatabase',
+        'USER': 'tutorial_hub',
+        'PASSWORD': 'tutorialhubadmin321',
+        'HOST': 'tutorialhubdb.cvzvordi3lvr.us-east-2.rds.amazonaws.com',
+        'PORT': 5432,
+
     }
 }
 
